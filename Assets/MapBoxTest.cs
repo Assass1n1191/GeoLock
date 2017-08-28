a095f734cfcb46dcc6135217613e85ccc3f8237a
Ivan Koval <koval.ivan.ua@gmail.com>
2017-08-28 10:19:05 +0300

    Initial commit

---
+++ Assets/MapBoxTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mapbox.Map;
+using Mapbox.Unity;
+using Mapbox.Unity.Utilities;
+using Mapbox.Utils;
+using Mapbox.Examples.LocationProvider;
+using Mapbox.Unity.Location;
+
+
+public class MapBoxTest : MonoBehaviour 
+{
+    public Text ResultText;
+    public Text CurrentPosition;
+
+    public EditorLocationProvider editorLocationProvider;
+    public DeviceLocationProvider deviceLocationProvider;
+
+    private Vector2d requiredPosition;
+    private double precision = 0.0005;
+        
+
+	private void Awake () 
+	{
+		
+	}
+	
+	private void Start () 
+	{
+        //requiredPosition = new Vector2d(49.443733, 32.056695); //Bidon
+        requiredPosition = new Vector2d(49.44333, 32.05922); //Ferma
+
+
+    }
+
+    private void Update ()
+	{
+        #if UNITY_EDITOR
+            CurrentPosition.text = editorLocationProvider.Location.ToString();
+            ResultText.gameObject.SetActive(CheckCurrentPosition(editorLocationProvider.Location));
+
+        #elif UNITY_ANDROID
+            CurrentPosition.text = deviceLocationProvider.Location.ToString();
+            ResultText.gameObject.SetActive(CheckCurrentPosition(deviceLocationProvider.Location));
+
+        #endif
+
+        
+
+        //#if UNITY_EDITOR
+        //        CurrentPosition.text = editorLocationProvider.Location.ToString();
+        //#elif UNITY_ANDROID
+        //        CurrentPosition.text = deviceLocationProvider.Location.ToString();
+        //#endif
+    }
+
+    private bool CheckCurrentPosition(Vector2d currentPos)
+    {
+        bool isInZone = true;
+
+        isInZone &= currentPos.x >= requiredPosition.x - precision 
+                    && currentPos.x <= requiredPosition.x + precision;
+
+        isInZone &= currentPos.y >= requiredPosition.y - precision
+                    && currentPos.y <= requiredPosition.y + precision;
+
+        return isInZone;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.SimpleAndroidNotifications;
+
+public class Player : MonoBehaviour 
+{
+    private bool _isInInteractiveZone = false;
+
+	private void Awake () 
+	{
+		
+	}
+	
+	private void Start () 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+		
+	}
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(!_isInInteractiveZone)
+            NotificationManager.SendWithAppIcon(new System.TimeSpan(0, 0, 0, 1), "Holy Shit!", "You arrived to an interactive zone!", Color.black);
+
+        _isInInteractiveZone = true;
+        //Debug.Log("Arrived");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isInInteractiveZone = false;
+    }
+}
