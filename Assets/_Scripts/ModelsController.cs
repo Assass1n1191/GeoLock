2d187089378d53910bef1aeaad83a20275d791ea
Inna Bakum <ibakum@mail.ru>
2017-08-28 15:15:47 +0300

    models scene

--- Assets/_Scripts/ModelsController.cs
+++ Assets/_Scripts/ModelsController.cs
@@ -1,17 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModelsController : MonoBehaviour {
 
 
-
+    public Text InfoAboytModels;
+    public GameObject Plane;
+    public GameObject ButtonOnModels;
+    public GameObject ButtonOffModels;
     //public List<GameObject> PlanetsGameObject = new List<GameObject>();
-    //GameObject _instantiateModels;
+    GameObject _instantiateModels;
 
     public void Show3DModels(InteractiveZone interactiveZone)
     {
-       // Instantiate();
+        ButtonOnModels.SetActive(false);
+        Plane.SetActive(true);
+        InfoAboytModels.text = interactiveZone.Description;
+       _instantiateModels = Instantiate(interactiveZone.Model);
     }
+
+    public void Destroy3DModels()
+    {
+        Destroy(_instantiateModels);
+        ButtonOnModels.SetActive(true);
+        Plane.SetActive(false);
+        InfoAboytModels.text = "";
+
+    }
+
+
+
+
+
+
+
 	
 }
