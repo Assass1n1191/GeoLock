d21eb55ae74244fa799b3c81faf27dd434d4782e
Ivan Koval <koval.ivan.ua@gmail.com>
2017-08-28 12:46:00 +0300

    Planets script

---
+++ Assets/_Scripts/Planet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour 
+{
+    private const float _rotateSpeed = 0.10f;
+    private Vector3 _rotateDirection;
+
+	private void Start () 
+	{
+        _rotateDirection = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
+	}
+	
+	private void Update () 
+	{
+        transform.Rotate(_rotateDirection * Time.deltaTime * _rotateSpeed);
+	}
+}
